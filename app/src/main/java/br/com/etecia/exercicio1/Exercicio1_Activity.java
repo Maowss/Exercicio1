4da312a04a87958afd71eccdca66026ee25be1c8
Maowss <Maowss@gmail.com>
2019-05-22 22:14:56 -0300

    Falta colocar os ícons nos botões, ajustar o layout, pegar as cores, porém toda estrutura está pronta. Fazer a Toolbar no JAVA também.

---
+++ app/src/main/java/br/com/etecia/exercicio1/Exercicio1_Activity.java
@@ -0,0 +1,13 @@
+package br.com.etecia.exercicio1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Exercicio1_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.exercicio1_layout);
+    }
+}
