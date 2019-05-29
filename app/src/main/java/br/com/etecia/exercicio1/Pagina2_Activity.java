231363192e4b9b9b450bc545365fec7da3ffa17f
Maowss <maowss@gmail.com>
2019-05-29 16:46:46 -0300

    Foi criada a nova página solicitada, faltam incluir as cores em ambas as páginas.

---
+++ app/src/main/java/br/com/etecia/exercicio1/Pagina2_Activity.java
@@ -0,0 +1,13 @@
+package br.com.etecia.exercicio1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Pagina2_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.pagina2_layout);
+    }
+}
