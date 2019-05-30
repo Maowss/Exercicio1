96dad50d0899f1a79d61dd39760140307650e858
Maowss <Maowss@gmail.com>
2019-05-29 22:12:23 -0300

    Terminamos o exercicio 1 + só falta terminar o border

--- app/src/main/java/br/com/etecia/exercicio1/Exercicio1_Activity.java
+++ app/src/main/java/br/com/etecia/exercicio1/Exercicio1_Activity.java
@@ -2,6 +2,9 @@ package br.com.etecia.exercicio1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 public class Exercicio1_Activity extends AppCompatActivity {
 
@@ -9,5 +12,20 @@ public class Exercicio1_Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.exercicio1_layout);
+
+        Toolbar toolbar = (Toolbar)findViewById(R.id.idToolbar);
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setTitle("Notes");
+        getSupportActionBar().setIcon(R.drawable.ic_keyboard_arrow_left);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.menu_app1, menu);
+        return super.onCreateOptionsMenu(menu);
     }
 }
